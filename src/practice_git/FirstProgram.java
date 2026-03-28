ffdadc8c6f356b98e1ae4f12464666de019691f6
anukratitiwari2000 <anukratitiwari2000@gmail.com>
2026-03-28 12:41:36 +0530

    Third Commit

--- src/practice_git/FirstProgram.java
+++ src/practice_git/FirstProgram.java
@@ -7,6 +7,9 @@ public static void main(String[] args) {
 	System.out.println("Make My Branch");
 	System.out.println("Switch to master Branch");
 
+	WebElement ele1=driver.findElement(By.id("Username"));
+	WebElement ele2=driver.findElement(By.id("Password"));
+	WebElement ele3=driver.findElement(By.id("LoginButton"));
 }
 
 }
