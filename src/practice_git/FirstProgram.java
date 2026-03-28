c6043f97201743547a26f7edbd70c52d8d614f3d
anukratitiwari2000 <anukratitiwari2000@gmail.com>
2026-03-28 11:48:07 +0530

    Hello

--- src/practice_git/FirstProgram.java
+++ src/practice_git/FirstProgram.java
@@ -3,7 +3,8 @@ package practice_git;
 public class FirstProgram {
 public static void main(String[] args) {
 	System.out.println("Hello GitBash");
-	System.out.println("Edit ti GitBash");
+	System.out.println("Edit t0 GitBash");
+	System.out.println("Make My Branch");
 
 }
 
