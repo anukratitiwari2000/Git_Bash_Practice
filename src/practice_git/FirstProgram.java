4c0fd1fefb3bee9f90c960540a005787c1408457
anukratitiwari2000 <anukratitiwari2000@gmail.com>
2026-03-28 11:27:26 +0530

    Second Commit

--- src/practice_git/FirstProgram.java
+++ src/practice_git/FirstProgram.java
@@ -3,9 +3,7 @@ package practice_git;
 public class FirstProgram {
 public static void main(String[] args) {
 	System.out.println("Hello GitBash");
-	System.out.println("Hello GitBash");
-	System.out.println("Hello GitBash");
-	System.out.println("Hello GitBash");
+	System.out.println("Edit ti GitBash");
 
 }
 
